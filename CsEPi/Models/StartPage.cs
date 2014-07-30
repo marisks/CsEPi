08b227d7f6ddeeae9a0c8c09f56452bb9de7adb5
Māris Krivtežs <marisks@gmail.com>
2014-07-30 23:21:07 +0300

    Install EPi and first model

---
+++ CsEPi/Models/StartPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace CsEPi.Models
+{
+    [ContentType(DisplayName = "Start page", GUID = "0E7FF2CB-C0C9-498D-A7EB-29310CA122E2", Order = 10, AvailableInEditMode = true)]
+    public class StartPage : PageData
+    {
+    }
+}
